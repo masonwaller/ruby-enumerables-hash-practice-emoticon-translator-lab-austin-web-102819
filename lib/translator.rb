fca6f1856fd12056d20bb755431dfeae529e073d
Mason Waller <masonwaller805@gmail.com>
2019-10-21 19:06:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(file_path, emoticon)
   if lists["get_emoticon"].include?(emoticon)
     return lists["get_emoticon"][emoticon]
   else
-    return "Sorry"
+    return "Sorry, that emoticon was not found"
   end
 end
 
